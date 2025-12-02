a59be4ec96893461d9a901bbb28673fa46ae8676
MoaazRaslan <moaaz0raslan@gmail.com>
2025-12-02 17:01:21 +0300

    finish password reset

--- apos/account/views.py
+++ apos/account/views.py
@@ -12,7 +12,7 @@ from django.urls import reverse
 from django.core.mail import send_mail
 from django.shortcuts import render,get_object_or_404
 from .models import User
-
+import re
 def verification_email(request,user):
     signer = TimestampSigner()
     email_token = signer.sign(user.id)
@@ -71,7 +71,7 @@ def verifyEmailView(request):
         return Response({"error":"token not found!!!!"},status=status.HTTP_400_BAD_REQUEST)
     signer = TimestampSigner()
     try :
-        user_id = signer.unsign(email_token,3)
+        user_id = signer.unsign(email_token,60)
     except SignatureExpired:
         return Response({"error":"token is expired"},status=status.HTTP_400_BAD_REQUEST)
     except BadSignature:
@@ -86,17 +86,72 @@ def verifyEmailView(request):
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
-def resetPassword(request):
+def resetPasswordRequest(request):
     email = request.data.get('email')
     if not email:
         return Response({"error":"enter email"},status=status.HTTP_400_BAD_REQUEST)
-    user = get_object_or_404(User,email = email)
+    if User.objects.filter(email = email).exists():
+        user = get_object_or_404(User,email = email)
+        signer = TimestampSigner()
+        email_token = signer.sign(user.id)
+        verify_url = request.build_absolute_uri(reverse("reset-password-verification"))+f"?token={email_token}"
+        message = f"hi {user} , to reset your password click on the following url \n {verify_url}"
+        send_mail("reset-password",message,"apostrophe@email.com",[user.email])
+    return Response({
+        "message":"email has been sent successfully"
+        },status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def verfiyResetToken(request):
+    email_token = request.query_params.get('token')
+    if not email_token:
+        return Response({
+            "error":"token not found"
+        },status=status.HTTP_400_BAD_REQUEST)
     signer = TimestampSigner()
-    email_token = signer.sign(user.id)
-    verify_url = request.build_absolute_uri(reverse("reset-password"))+f"?token={email_token}"
-    message = f"hi {user} , to reset your password click on the following url \n {verify_url}"
-    send_mail("reset-password",message,"apostrophe@email.com",[user.email])
-    return Response(
-        {"message":"email has been sent successfully"},
-        status=status.HTTP_200_OK
-        )
+    try :
+        user_id = signer.unsign(email_token,60)
+
+    except SignatureExpired:
+        return Response({"error":"token expired"},status=status.HTTP_400_BAD_REQUEST)
+    except BaseException:
+        return Response({"error":"invalid token"},status=status.HTTP_400_BAD_REQUEST)
+        
+    return Response({"message":"token is valid","user_id":user_id},
+                    status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def resetPassword(request):
+    email_token = request.data.get('token')
+    password = request.data.get('password')
+    password2 = request.data.get('password2')
+    if not email_token or not password or not password2:
+        return Response({"error":"toke and passwords are reuquired"},status=status.HTTP_400_BAD_REQUEST)
+    signer = TimestampSigner()
+    try:
+        user_id = signer.unsign(email_token,60)
+
+    except SignatureExpired:
+        return Response({"error":"token expired"},status=status.HTTP_400_BAD_REQUEST)
+    except BaseException:
+        return Response({"error":"invalid token"},status=status.HTTP_400_BAD_REQUEST)
+
+    user = get_object_or_404(User,id = user_id)
+    if password != password2:
+        return Response({"error":"password and password2 should be matched"},status=status.HTTP_400_BAD_REQUEST)
+    if len(password) < 8 or len(password)>16:
+        return Response({"error":"password should be at least 8 chars and at most 16 chars"},status=status.HTTP_400_BAD_REQUEST)
+    if not re.search(r"[A-Z]",password):
+        return Response({"error":"password should have at least one capital letter"},status=status.HTTP_400_BAD_REQUEST)
+    if not re.search(r"[a-z]",password):
+        return Response({"error":"password should have at least one small letter"},status=status.HTTP_400_BAD_REQUEST)
+    if not re.search(r"[0-9]",password):
+        return Response({"error":"password should have at least one number"},status=status.HTTP_400_BAD_REQUEST)
+    user.set_password(password)
+    user.save()
+    return Response({"message":"passwrod reset succesfully "},status=status.HTTP_200_OK)
+
+
